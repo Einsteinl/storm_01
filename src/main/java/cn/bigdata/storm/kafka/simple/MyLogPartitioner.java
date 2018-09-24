145c3c8f4e88efcc52e7168daee9f59a55dc20e6
leishao <1019990958@qq.com>
2018-09-24 15:21:14 +0800

    second

--- src/main/java/cn/bigdata/storm/kafka/simple/MyLogPartitioner.java
+++ src/main/java/cn/bigdata/storm/kafka/simple/MyLogPartitioner.java
@@ -1,8 +1,13 @@
 package cn.bigdata.storm.kafka.simple;
 
 
+import kafka.producer.Partitioner;
 
 public class MyLogPartitioner implements Partitioner {
+    @Override
+    public int partition(Object o, int i) {
+        return 0;
+    }
 
-    private static Logger
+    //private static Logger
 }
